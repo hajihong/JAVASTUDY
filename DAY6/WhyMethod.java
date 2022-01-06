cc18e41018501b653e79becb053d4a88c39c7977
hajihong <hajihong96@gmail.com>
2022-01-06 16:14:13 +0900

    DAY6 second Commit

---
+++ DAY6/WhyMethod.java
@@ -0,0 +1,20 @@
+public class WhyMethod {
+	
+	public static void main(String[] args) {
+		
+		//100000
+		printTwoTimesA();
+		
+		//100000
+		printTwoTimesA();
+		
+		//100000
+		printTwoTimesA();
+	}
+	
+	public static void printTwoTimesA() {
+		System.out.println("-");
+		System.out.println("a");
+		System.out.println("a");
+	}
+}
