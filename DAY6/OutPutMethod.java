6c06c8f41a7aea1b8fec1d8271014b9e433a7ebe
hajihong <hajihong96@gmail.com>
2022-01-06 16:20:50 +0900

    Day6 fourth Commit

---
+++ DAY6/OutPutMethod.java
@@ -0,0 +1,17 @@
+
+public class OutPutMethod {
+	
+	public static String a() {
+		return "a";
+	}
+	
+	public static int one() {
+		return 1;
+	}
+	
+	public static void main(String[] args) {
+		
+		System.out.println(a());
+		System.out.println(one());
+	}
+}
